220bfe18f075969f06fcdbe7b7da15885bb11fa2
VadymNeiman <vadimnejman1965@gmail.com>
2024-01-09 20:53:59 +0200

    Виконано завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const arrayOfIngredients = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList = "item";
+  itemEl.textContent = ingredient;
+  return itemEl;
+});
+
+const ingredientsEl = document.querySelector("#ingredients");
+ingredientsEl.append(...arrayOfIngredients);
