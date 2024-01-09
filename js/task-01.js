75127b9e072c8a421d9d8ca68e41984b48ad19e6
VadymNeiman <vadimnejman1965@gmail.com>
2024-01-09 16:32:39 +0200

    Update task-01.js
    Видалено проміжні закоментовані варіанти

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,7 @@
-// console.log(
-//   "Number of categories: ",
-//   document.querySelectorAll(".item").length
-// );
-
 const listOfCategories = document.querySelectorAll(".item");
 
 console.log("Number of categories: ", listOfCategories.length);
 console.log("");
-
-// for (let i = 0; i < listOfCategories.length; i += 1) {
-//   console.log("Category: ", listOfCategories[i].firstElementChild.textContent);
-// }
-
-// for (const categorie of listOfCategories) {
-//   console.log("Category: ", categorie.firstElementChild.textContent);
-//   console.log("Elements: ", categorie.lastElementChild.children.length);
-//   console.log("");
-// }
 
 listOfCategories.forEach(function (categorie) {
   console.log("Category: ", categorie.firstElementChild.textContent);
