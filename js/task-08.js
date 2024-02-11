64fd7c4e077c3ef7c7ac9970084c025f66e1f897
VadymNeiman <vadimnejman1965@gmail.com>
2024-02-11 20:21:43 +0200

    Виконані завдання 8 та 9.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", onSubmit);
 
+function onSubmit(event) {
+  event.preventDefault();
+  const userForm = event.currentTarget;
+  const { email, password } = userForm.elements;
+
+  if (email.value.length === 0 || password.value.length === 0) {
+    alert("Всі поля мають бути заповнені!");
+  }
+
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(userData);
+  userForm.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function onClickColorChange() {
+  const page = document.querySelector("body");
+  const colorValue = document.querySelector(".color");
+
+  const currentColor = getRandomHexColor();
+
+  page.style.backgroundColor = currentColor;
+  colorValue.textContent = currentColor;
+}
+
+const btnChangeColor = document.querySelector(".change-color");
+btnChangeColor.addEventListener("click", onClickColorChange);
